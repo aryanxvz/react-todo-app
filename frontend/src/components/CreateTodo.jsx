db19a43f3701d944a805d20f10b67b6cfc58b0f3
Aryan Mane <aryanmane7916@gmail.com>
2024-02-18 16:59:42 +0530

    defined frontend

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,8 @@
+
+export function CreateTodo() {
+    return <div>
+        <input type="text" placeholder="tilte"></input> <br></br>
+        <input type="text" placeholder="description"></input> <br></br>
+        <button>Add Todo</button> 
+    </div>
+}
